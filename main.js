47d90da2546afc840ef58669f8a220ded6c733c6
StacyGilbert7713 <63259897+StacyGilbert7713@users.noreply.github.com>
2020-08-18 22:51:38 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 class Traveler {
-    constructor (name) {
+    constructor(name) {
         this.name = name
         this.food = 1
         this.isHealthy = true
     }
 
-    hunt () {
-    return this.food += 2
+    hunt() {
+        return this.food += 2
     }
 
-    eat () {
+    eat() {
         if (this.food > 0) {
-            this.food --
+            this.food--
         } else {
             this.isHealthy = false
         }
@@ -20,41 +20,34 @@ class Traveler {
 }
 
 class Wagon {
-    constructor (capacity) {
+    constructor(capacity) {
         this.capacity = capacity
         this.passenger = []
     }
 
-      getAvailableSeatCount() {
-        if (this.passenger.length <= 2) {
-            return this.capacity - this.passenger.length
-        } else {
-            return 0
+    getAvailableSeatCount() {
         return (this.capacity - this.passenger.length)
+    }
+
+    join(traveler) {
+        if (this.getAvailableSeatCount() == 0) {
+            return 'Wagon is full.'
+        } else {
+            return this.passenger.push(traveler)
         }
     }
 
-    join (traveler) {
-        if (this.getAvailableSeatCount () == 0) {
-           return 'no space left' 
-        if (this.getAvailableSeatCount () <= 0) {
-           return 'Wagon is full.' 
-        } else {
-        return this.passenger.push (traveler)
-        }
-   } 
-            
-    shouldQuarantine () {
-        const sick = this.passenger.some (passenger =>
+    shouldQuarantine() {
+        const sick = this.passenger.some(passenger =>
             passenger.isHealthy === false)
-       return sick
-    } 
+        return sick
+    }
 
-    totalFood () {
-        const food = this.passenger.map (traveler => traveler.food)
-        let totalFood = 0
+    totalFood() {
+        const food = this.passenger.map(traveler => traveler.food)
+        let totalFood = 4
         for (let index = 0; index < food.length; index++) {
-            totalFood = totalFood += food [index]
+            totalFood = totalFood += food[index]
         }
         return totalFood
     }
